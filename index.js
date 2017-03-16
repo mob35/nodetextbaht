bba9862b104da6e3f62ae6f6c67447cd5b0b3976
วัจน์กร จงกลาง < >
2017-03-16 15:11:21 +0700

    node textbaht

---
+++ index.js
@@ -0,0 +1,9 @@
+var exports = module.exports = {};
+
+exports.sq = function (width, height) {
+  return width * height;
+};
+
+exports.tri = function (width, height) {
+  return (width * height) * 0.5;
+}; 
